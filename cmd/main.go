df5bee53cb97805ee89c078c8f813463fe21c074
Anthony Waye <anthony.waye@riotinto.com>
2025-05-06 09:49:23 +1000

    fix: added debug flag

--- cmd/main.go
+++ cmd/main.go
@@ -59,6 +59,7 @@ func main() {
 	var secureMetrics bool
 	var enableHTTP2 bool
 	var tlsOpts []func(*tls.Config)
+	var debug bool
 	flag.StringVar(&metricsAddr, "metrics-bind-address", "0", "The address the metrics endpoint binds to. "+
 		"Use :8443 for HTTPS or :8080 for HTTP, or leave as 0 to disable the metrics service.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
@@ -69,8 +70,9 @@ func main() {
 		"If set, the metrics endpoint is served securely via HTTPS. Use --metrics-secure=false to use HTTP instead.")
 	flag.BoolVar(&enableHTTP2, "enable-http2", false,
 		"If set, HTTP/2 will be enabled for the metrics and webhook servers")
+	flag.BoolVar(&debug, "debug", false, "Enable debug-level logging")
 	opts := zap.Options{
-		Development: true,
+		Development: debug,
 	}
 	opts.BindFlags(flag.CommandLine)
 	flag.Parse()
