53cb1e448cab348cec15e42b5e7bba23f6afbe3e
Anthony Waye <anthony.waye@riotinto.com>
2025-05-07 15:04:05 +1000

    fix: removed dev mode

--- cmd/main.go
+++ cmd/main.go
@@ -71,9 +71,7 @@ func main() {
 	flag.BoolVar(&enableHTTP2, "enable-http2", false,
 		"If set, HTTP/2 will be enabled for the metrics and webhook servers")
 
-	opts := zap.Options{
-		Development: true,
-	}
+	opts := zap.Options{}
 
 	opts.BindFlags(flag.CommandLine)
 	flag.Parse()
